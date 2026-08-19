ca5e9c5a25c3f49586311c2b1a14b8039301d9be
duck5583479 <cant5583@duck.com>
2026-08-19 11:25:43 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -85,7 +85,7 @@ const getCurrentColo = async () => {
 };
 const textEncoder = new TextEncoder(), textDecoder = new TextDecoder();
 const panelHtmlUrl = 'https://1345695.github.io/index-404-html/panel';
-const errorHtmlUrl = 'https://edcloudwasm.samcost200.workers.dev/404.html';
+const errorHtmlUrl = 'https://cf.090227.xyz';
 import wasmModule from './protocol.wasm';
 const instance = new WebAssembly.Instance(wasmModule);
 const {
